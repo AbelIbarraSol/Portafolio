472f5d01ef88abc110daf5d83714aa4bb4d3001f
Abel Ibarra Solgorre <aibarrasol@outlook.com>
2023-06-12 19:45:37 -0500

    Tercer commit: Modificaciones realizadas en skills, proyectos y imagen pricipal

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById("container");
-
-container.addEventListener('click', (e) => {
- if(e.target.classList.contains('skill__content_list-title')){
-   e.target.parentElement.classList.toggle('scale')
-   e.target.children[2].classList.toggle('rotate')
- }
-
-});
